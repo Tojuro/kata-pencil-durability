cf29b4d1ec9393c2dc1e6740b45c46a952c21ac8
Tojuro <thomas.j.rodgers@live.com>
2019-06-08 00:06:12 -0400

    Point Degradation Section

--- src/Kata.PencilService.Tests/PencilTests.cs
+++ src/Kata.PencilService.Tests/PencilTests.cs
@@ -18,14 +18,69 @@ namespace Kata.PencilService.Tests
         }
 
         [Fact]
-        public void WhenWriteToPaperTheContentIsAppended()
+        public void CanCreatePencilAndSetDurability()
+        {
+            var pencil = new Pencil(100);
+
+            Assert.Equal(100, pencil.Durability);
+        }
+
+        [Theory, 
+            InlineData(""),
+            InlineData(TestText2),]
+        public void WhenWritingToPaperTheContentIsAppended(string paperInitialContent)
         {
             var pencil = new Pencil();
-            var paper = new Paper();
-            
+            var paper = new Paper { Content = paperInitialContent };
+
             pencil.Write(TestText1, ref paper);
             
-            Assert.Equal(TestText1, paper.Content);
+            Assert.Equal(string.Concat(paperInitialContent, TestText1), paper.Content);
         }
+
+        [Theory,
+            InlineData("o", 1),
+            InlineData("O", 2),
+            InlineData("abcde", 5),
+            InlineData("ABCDE", 10),
+            InlineData("ABCDEfghij", 15),
+            InlineData("abcdefghijklmnopqrstuvwxyz", 26),
+            InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 52),
+            InlineData("AzAzByBy", 12),]
+        public void WhenWritingThePointDegrades(string text, int degradation)
+        {
+            var durabilityStart = 100;
+            var pencil = new Pencil(durabilityStart);
+            var paper = new Paper();
+
+            pencil.Write(text, ref paper);
+
+            Assert.Equal(durabilityStart-degradation, pencil.Durability);
+        }
+
+        [Theory,
+            InlineData("o", "o"),
+            InlineData("O", "O"),
+            InlineData("abcdef", "abcdef"),
+            InlineData("abcdefghij", "abcdefghij"),
+            InlineData("abcdefghijklmno", "abcdefghij&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"),
+            InlineData("ABCDE", "ABCDE"),
+            InlineData("ABCDEFGHIJ", "ABCDE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"),
+            InlineData("AAAAaaaa", "AAAAaa&nbsp;&nbsp;"),
+            InlineData("aaaaAAAA", "aaaaAAA&nbsp;"),
+            InlineData("a test", "a&nbsp;test"),
+            InlineData("i am ok", "i&nbsp;am&nbsp;ok"),
+            InlineData("a a a   ", "a&nbsp;a&nbsp;a&nbsp;&nbsp;&nbsp;"),]
+        public void WhenWritingToPaperOnlyBlankSpacesAreShownIfDurabilityIsLessOrEqualZero(string text, string result)
+        {
+            var durabilityStart = 10;
+            var pencil = new Pencil(durabilityStart);
+            var paper = new Paper();
+
+            pencil.Write(text, ref paper);
+
+            Assert.Equal(result, paper.Content);
+        }
+
     }
 }
--- src/Kata.PencilService/Pencil.cs
+++ src/Kata.PencilService/Pencil.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Text;
 
 namespace Kata.PencilService
 {
     public class Pencil
     {
+        public int Durability {get; private set;}
+
+        private const string NonBreakSpace = "&nbsp;";
+
+        public Pencil (int durability = 1000)
+        {
+            Durability = durability;
+        }
+
         public void Write (string text, ref Paper paper)
         {
-            paper.Content = string.Concat(paper.Content, text);
+            var displayText = ApplyTextDegradation(text);
+            paper.Content = string.Concat(paper.Content, displayText);
         }
+
+        private string ApplyTextDegradation(string text)
+        {
+            var displayText = new StringBuilder();
+            var chrs = text.ToCharArray();
+
+            foreach(var chr in chrs)
+            {
+                // NOTE: treating lowercase (per spec), numbers and special characters == 1
+                var ptValue = (!char.IsWhiteSpace(chr) ? 1 : 0) + (char.IsUpper(chr) ? 1 : 0);
+                displayText.Append(PointDegradation(chr, ptValue));
+            }
+
+            return displayText.ToString();
+        }
+
+        private string PointDegradation(char chr, int ptVal)
+        {
+            var canWrite = Durability >= ptVal && ptVal > 0;
+            var isSpace = char.IsWhiteSpace(chr);
+            Durability = Math.Max(0, Durability - ptVal);
+
+            return canWrite && !isSpace ? chr.ToString() : NonBreakSpace;
+        }
+
     }
 }
